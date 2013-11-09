9c074d9a5dcd81b2f49dbc6a50f3b0a991faed42
XsMagical <XsMagical@team-nocturnal.net>
2013-11-09 08:19:35 -0500

    Update ska.mk

--- ska.mk
+++ ska.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/jflteatt/full_jflteatt.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/ska/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/ska/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteuc TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
 
